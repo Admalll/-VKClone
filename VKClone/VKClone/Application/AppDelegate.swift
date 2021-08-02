20086f282451692ae7055f2674a14bfe99e45303
Admalll <F1zzer1997195@gmail.com>
2021-08-02 20:04:50 +0300

    Init project

---
+++ VKClone/VKClone/Application/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  VKClone
+//
+//  Created by Vlad on 02.08.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
+
---
+++ VKClone/VKClone/Controller/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  VKClone
+//
+//  Created by Vlad on 02.08.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
