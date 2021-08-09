98b8fd842476bef1d860bc2f1ff5acf03f4ceb8a
Admalll <F1zzer1997195@gmail.com>
2021-08-09 22:51:31 +0300

    Реализовал 6 задание

---
+++ VKClone/VKClone/Controller/NewsViewController.swift
@@ -0,0 +1,19 @@
+// NewsViewController.swift
+// Copyright © Roadmap. All rights reserved.
+
+import UIKit
+
+final class NewsViewController: UIViewController { }
+
+// MARK: - UITableViewDataSource
+
+extension NewsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell") else { return UITableViewCell() }
+        return cell
+    }
+}
