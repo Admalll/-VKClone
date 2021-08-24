8253066de6e894ea267df3544a84deddcb53ffe6
Admalll <F1zzer1997195@gmail.com>
2021-08-24 10:11:43 +0300

    Правки по стайлингу

--- VKClone/VKClone/Session.swift
+++ VKClone/VKClone/Session.swift
@@ -1,14 +1,8 @@
-//
-//  Session.swift
-//  VKClone
-//
-//  Created by Vlad on 23.08.2021.
-//
-
 import Foundation
-class Session {
+
+final class Session {
     static let instance = Session()
     private init(){}
     var token = ""
-    var userID : Int = 0
+    var userID: Int = 0
 }
